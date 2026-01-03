7a970e8ffe6610516c51ebb77b79fc3c559314a4
Maciej Mazurek <mazurekk67@gmail.com>
2026-01-03 16:40:27 +0100

    Initial blank window

--- gk1-3D-scene-renderer.h
+++
@@ -1,8 +0,0 @@
-﻿// gk1-3D-scene-renderer.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
