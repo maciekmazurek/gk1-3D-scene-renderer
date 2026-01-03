627451654081e001b3755d3fb1213b91f5ddb017
Maciej Mazurek <mazurekk67@gmail.com>
2026-01-03 15:48:21 +0100

    Add project files.

---
+++ gk1-3D-scene-renderer.cpp
@@ -0,0 +1,12 @@
+﻿// gk1-3D-scene-renderer.cpp : Defines the entry point for the application.
+//
+
+#include "gk1-3D-scene-renderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
