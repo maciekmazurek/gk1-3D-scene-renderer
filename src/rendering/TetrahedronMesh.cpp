cc290fcba8c60b20b94c99558859cce066918cd0
Maciej Mazurek <mazurekk67@gmail.com>
2026-01-07 11:00:29 +0100

    Lab exercise part 1

--- src/rendering/TetrahedronMesh.cpp
+++ src/rendering/TetrahedronMesh.cpp
@@ -8,19 +8,47 @@ namespace gk1
 {
     namespace
     {
-        constexpr std::array<float, 24> VERTICES = {
-            // positions          // colors
-             1.0F,  1.0F,  1.0F,  1.0F, 0.0F, 0.0F, // v0 - red
-            -1.0F, -1.0F,  1.0F,  0.0F, 1.0F, 0.0F, // v1 - green
-            -1.0F,  1.0F, -1.0F,  0.0F, 0.0F, 1.0F, // v2 - blue
-             1.0F, -1.0F, -1.0F,  1.0F, 1.0F, 0.0F  // v3 - yellow
+        constexpr std::array<float, 144> VERTICES = {
+            // positions           // colors
+            // Przód - czerwony
+             1.0F,  1.0F,  1.0F,   1.0F, 0.0F, 0.0F,
+            -1.0F,  1.0F,  1.0F,   1.0F, 0.0F, 0.0F,
+            -1.0F, -1.0F,  1.0F,   1.0F, 0.0F, 0.0F,
+             1.0F, -1.0F,  1.0F,   1.0F, 0.0F, 0.0F,
+            // Ty³ - zielony
+             1.0F,  1.0F, -1.0F,   0.0F, 1.0F, 0.0F,
+            -1.0F,  1.0F, -1.0F,   0.0F, 1.0F, 0.0F,
+            -1.0F, -1.0F, -1.0F,   0.0F, 1.0F, 0.0F,
+             1.0F, -1.0F, -1.0F,   0.0F, 1.0F, 0.0F,
+            // Lewo - niebieski
+            -1.0F,  1.0F,  1.0F,   0.0F, 0.0F, 1.0F,
+            -1.0F,  1.0F, -1.0F,   0.0F, 0.0F, 1.0F,
+            -1.0F, -1.0F, -1.0F,   0.0F, 0.0F, 1.0F,
+            -1.0F, -1.0F,  1.0F,   0.0F, 0.0F, 1.0F,
+            // Prawo - ¿ó³ty
+             1.0F,  1.0F, -1.0F,   1.0F, 1.0F, 0.0F,
+             1.0F,  1.0F,  1.0F,   1.0F, 1.0F, 0.0F,
+             1.0F, -1.0F,  1.0F,   1.0F, 1.0F, 0.0F,
+             1.0F, -1.0F, -1.0F,   1.0F, 1.0F, 0.0F,
+            // Góra - magenta
+             1.0F,  1.0F, -1.0F,   1.0F, 0.0F, 1.0F,
+            -1.0F,  1.0F, -1.0F,   1.0F, 0.0F, 1.0F,
+            -1.0F,  1.0F,  1.0F,   1.0F, 0.0F, 1.0F,
+             1.0F,  1.0F,  1.0F,   1.0F, 0.0F, 1.0F,
+            // Dó³ - cyjan
+             1.0F, -1.0F,  1.0F,   0.0F, 1.0F, 1.0F,
+            -1.0F, -1.0F,  1.0F,   0.0F, 1.0F, 1.0F,
+            -1.0F, -1.0F, -1.0F,   0.0F, 1.0F, 1.0F,
+             1.0F, -1.0F, -1.0F,   0.0F, 1.0F, 1.0F
         };
 
-        constexpr std::array<unsigned int, 12> INDICES = {
-            0, 1, 2,
-            0, 3, 1,
-            0, 2, 3,
-            1, 3, 2
+        constexpr std::array<unsigned int, 36> INDICES = {
+             0,  1,  2,  0,  2,  3, // Przód
+             4,  6,  5,  4,  7,  6, // Ty³
+             8,  9, 10,  8, 10, 11, // Lewo
+            12, 14, 13, 12, 15, 14, // Prawo
+            16, 17, 18, 16, 18, 19, // Góra
+            20, 22, 21, 20, 23, 22  // Dó³
         };
     } // namespace
 
